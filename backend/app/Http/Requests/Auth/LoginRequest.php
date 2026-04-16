480d0dba2082071e8c7641283a6941cbfcad87ac
excotide <labiqjazz@gmail.com>
2026-04-16 08:20:57 +0700

    login register

--- backend/app/Http/Requests/Auth/LoginRequest.php
+++ backend/app/Http/Requests/Auth/LoginRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests\Auth;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class LoginRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,9 +23,6 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name'  => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'unique:user.email'],
-            'password'  => ['required', 'min:8', 'confirmed']
         ];
     }
 }
